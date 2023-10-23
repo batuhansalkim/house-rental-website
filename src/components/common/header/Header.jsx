de9306a54f12993868adeceba489d24bb31322d0
batuhan <batusalkim11@gmail.com>
2023-10-24 02:40:48 +0300

    navbar kısmı tamamlandı responsive yapıldı

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./header.css";
 import { Link } from "react-router-dom"
 import nav from "../../data/nav";
+import "../../../App.css";
 
 function Header() {
+    const [navlist,setNavList] = useState(false);
+
   return (
     <>
     <header>
@@ -12,14 +15,25 @@ function Header() {
                 <img src="./images/logo.png" alt="" />
             </div>
             <div className="nav">
-                <ul>
+                <ul className={navlist ? "small" : "flex"}>
                     {nav.map((list,index)=>(
                         <li key={index}>
                             <Link to={list.path}>{list.text}</Link>
                         </li>
                     ))}
                 </ul>
-                
+            </div>
+            <div className="button flex">
+                <h4><span>2</span>My List</h4>
+                <button className='btn1'>
+                    <i className='fa fa-sign-out'></i>Sign In
+                </button>
+            </div>
+
+            <div className='toggle'>
+                <button onClick={()=>setNavList(!navlist)}>
+                   {navlist ?  <i className='fa fa-times'></i> :  <i className='fa fa-bars'></i>}
+                </button>
             </div>
         </div>
     </header>
