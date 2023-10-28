00ca692701108e6da17a3db084e3a1f47f0deba2
batuhan <batusalkim11@gmail.com>
2023-10-29 02:29:04 +0300

    featured baslandi

--- src/components/home/featured/Featured.jsx
+++ src/components/home/featured/Featured.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./featured.css";
+import Heading from "../../common/header/Heading";
 
 export default function Featured() {
   return (
     <>
-        Featured
+      <section className='featured background'>
+        <div className="container">
+          <Heading title="Featured Property Types" subtitle="Find All Type of Property."/>
+        </div>
+      </section>
     </>
   )
 }
